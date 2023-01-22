42cf2419d330750385ca6fa4d4312c5cda95aba8
Max Große <github@maxrn.net>
2023-01-22 17:52:06 +0100

    feat: finished map with polygon

--- src/App.tsx
+++ src/App.tsx
@@ -15,20 +15,12 @@ async function sendToDrone(): Promise<string> {
 }
 
 function App() {
-    const [count, setCount] = useState(0)
     const [loading, setLoading] = useState<boolean>(false);
 
     return (
         <div className="App">
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src="/vite.svg" className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://reactjs.org" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
+            <h1>FireFlyer Dashboard</h1>
+            <div id="map"></div>
             <MapComponent />
             <div className="card">
                 <button onClick={async () => {
@@ -36,15 +28,9 @@ function App() {
                     await sendToDrone();
                     setLoading(false);
                 }}>
-                    {loading ? "Sending..." : "Send data to firemen."}
+                    {loading ? "Sending..." : "Send data to fire flyer."}
                 </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
             </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
         </div>
     )
 }
--- src/MapComponent.tsx
+++ src/MapComponent.tsx
@@ -1,4 +1,7 @@
 import GoogleMapReact from 'google-map-react';
+import { useRef, useState } from 'react';
+import { Icon } from '@iconify/react';
+import * as d3 from 'd3';
 
 type markerProps = {
     lat: number
@@ -6,7 +9,36 @@ type markerProps = {
     text: string
 }
 
-const AnyReactComponent = ({ text }: markerProps) => <div>{text}</div>;
+const Marker = ({ text }: markerProps) => {
+    return (
+        // "hello"
+        <Icon icon="ph:map-pin-fill" height={"20"} />
+    )
+}
+
+type onClickProps = {
+    lat: number
+    lng: number
+    event: string
+}
+
+type Coord = {
+    lat: number
+    lng: number
+}
+
+function drawPolygon(coords: Coord[], API: any, map: any) {
+    const poly = new API.current.Polygon({
+        paths: coords,
+        strokeColor: "#FF0000",
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: "#FF0000",
+        fillOpacity: 0.35,
+    });
+
+    poly.setMap(map.current);
+}
 
 export default function SimpleMap() {
     const defaultProps = {
@@ -17,20 +49,46 @@ export default function SimpleMap() {
         zoom: 11
     };
 
+    const mapsAPIref: any = useRef(null);
+    const mapRef: any = useRef(null);
+
+    const [corners, setCorners] = useState<{ lat: number, lng: number }[]>([]);
+
+    function onClickOnMap({ lat, lng, event }: onClickProps) {
+        setCorners((oldState: { lat: number, lng: number }[]) => [...oldState, { lat, lng }])
+        console.log(lat, lng, event)
+    }
+    type APIProps = {
+        map: any
+        maps: any
+        ref: any
+    }
+
+    function googleAPILoaded({ map, maps }: APIProps) {
+        mapsAPIref.current = maps
+        mapRef.current = map
+    }
+
     return (
         // Important! Always set the container height explicitly
-        <div style={{ height: '80vh', width: '100%' }}>
+        <div style={{ height: '80vh', width: '60vw' }}>
             <GoogleMapReact
+                onClick={onClickOnMap}
                 bootstrapURLKeys={{ key: "AIzaSyAj0-b3jl0qxB9vatFCSy9k5WlZsW_k7p4" }}
                 defaultCenter={defaultProps.center}
                 defaultZoom={defaultProps.zoom}
+                yesIWantToUseGoogleMapApiInternals
+                onGoogleApiLoaded={googleAPILoaded}
             >
-                <AnyReactComponent
-                    lat={50.9064509}
-                    lng={14.3204418}
-                    text="My Marker"
-                />
+                {corners.map((pos, i) => (
+                    <Marker
+                        lat={pos.lat + 0.0045}
+                        lng={pos.lng - 0.003}
+                        text={"Marker " + (i + 1)}
+                    />
+                ))}
             </GoogleMapReact>
+            <button onClick={() => drawPolygon(corners, mapsAPIref, mapRef)}>Draw polygon</button>
         </div>
     );
 }
