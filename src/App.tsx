f9cb33626e8edd729bedb576933d6492fa57e69d
Max Große <github@maxrn.net>
2023-01-25 19:27:01 +0100

    feat: add search area list

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,98 @@
-import { Dispatch, SetStateAction, useState } from 'react'
-import './App.css'
-import MapComponent from './MapComponent'
+import { Dispatch, SetStateAction, useState } from "react";
+import "./App.css";
+import { SimpleMap } from "./MapComponent";
 
 async function sendDronePromise(): Promise<string> {
-    const sending = new Promise<string>((resolve) => {
-        setTimeout(() => {
-            console.log("done sending...")
-            resolve("sent successfully!");
-        }, 3000);
-    })
+  const sending = new Promise<string>((resolve) => {
+    setTimeout(() => {
+      console.log("done sending...");
+      resolve("sent successfully!");
+    }, 3000);
+  });
 
-    return sending;
+  return sending;
 }
 
 function App() {
-    const [loading, setLoading] = useState<boolean>(false);
-    const [dronesInUse, setDronesInUse] = useState(3);
+  const [loading, setLoading] = useState<boolean>(false);
+  const MAX_DRONES = 7;
+  const [dronesInUse, setDronesInUse] = useState(0);
+  const dronesAvailable = MAX_DRONES - dronesInUse;
+  const [polys, setPolys] = useState<Polygon[]>([]);
 
-    const MAX_DRONES = 7
+  const SEARCH_TIME_MIN = 8 * 60;
 
-    return (
-        <div className="App">
-            <h1>FireFlyer Dashboard</h1>
-            <div id="map"></div>
-            <MapComponent />
-            <div className="card">
-                <button style={{margin: "1em"}} onClick={() => sendToDrone(setLoading, setDronesInUse)}>
-                    {loading ? "Sending..." : "Send data to fire flyer."}
-                </button>
-                <span>{MAX_DRONES - dronesInUse} drones available / {dronesInUse} drones surveying forest</span>
-            </div>
+  return (
+    <div className="App">
+      <h1>FireFlyer Dashboard</h1>
+      <div id="map"></div>
+      <SimpleMap polys={polys} setPolys={setPolys} />
+      <div className="card">
+        <button
+          style={{ margin: "1em" }}
+          onClick={() => sendToDrone(setLoading)}
+        >
+          {loading ? "Sending..." : "Send data to fire flyer."}
+        </button>
+        <span>
+          {MAX_DRONES - dronesInUse} drones available / {dronesInUse} drones
+          surveying forest
+        </span>
+      </div>
+      {polys.map((poly) => (
+        <div key={poly.name}>
+          <div>
+            Search Area {poly.name}, drones: {poly.amntDrones}, est time:{" "}
+            {poly.amntDrones === 0
+              ? "Please assign at least one drone to this search area."
+              : (SEARCH_TIME_MIN / poly.amntDrones).toString() + "min"}
+          </div>
+          <button
+            disabled={dronesAvailable === MAX_DRONES || poly.amntDrones === 0}
+            onClick={() => updateDroneCount(poly, setPolys, -1, setDronesInUse)}
+          >
+            -1
+          </button>
+          <button
+            disabled={dronesAvailable === 0}
+            onClick={() => updateDroneCount(poly, setPolys, 1, setDronesInUse)}
+          >
+            +1
+          </button>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
 
-async function sendToDrone(setLoading: any, setDronesInUse: Dispatch<SetStateAction<number>>) {
-    setLoading(true);
-    await sendDronePromise();
-    setLoading(false);
-    setDronesInUse((drones) => drones + 1)
+async function sendToDrone(
+  setLoading: any
+) {
+  setLoading(true);
+  await sendDronePromise();
+  setLoading(false);
 }
 
-export default App
+function updateDroneCount(
+  poly: Polygon,
+  setPolys: Dispatch<SetStateAction<Polygon[]>>,
+  amount: number,
+  setDronesInUse: Dispatch<SetStateAction<number>>
+) {
+  poly.amntDrones = poly.amntDrones + amount;
+  setDronesInUse((drones) => drones + amount);
+  setPolys((polys) => [...polys]);
+}
+
+export type Polygon = {
+  path: Coordinate[];
+  name: string;
+  amntDrones: number;
+};
+
+export type Coordinate = {
+  lat: number;
+  lng: number;
+};
+
+export default App;
--- src/MapComponent.tsx
+++ src/MapComponent.tsx
@@ -1,106 +1,140 @@
-import GoogleMapReact from 'google-map-react';
-import { useRef, useState } from 'react';
-import { Icon } from '@iconify/react';
-import * as d3 from 'd3';
-
-type markerProps = {
-    lat: number
-    lng: number
-    text: string
-}
-
-const Marker = ({ text }: markerProps) => {
-    return (
-        // "hello"
-        <Icon icon="ph:map-pin-fill" height={"20"} />
-    )
-}
+import GoogleMapReact from "google-map-react";
+import { Dispatch, SetStateAction, useRef, useState } from "react";
+import { Coordinate, Polygon } from "./App";
 
 type onClickProps = {
-    lat: number
-    lng: number
-    event: string
+  lat: number;
+  lng: number;
+  event: string;
+};
+
+function getCenterOfPolygon(coords: Coordinate[]): Coordinate {
+  const l = coords.length;
+
+  const sum = coords.reduce((acc, curr) => ({
+    lat: acc.lat + curr.lat,
+    lng: acc.lng + curr.lng,
+  }));
+
+  return { lat: sum.lat / l, lng: sum.lng / l };
 }
 
-function drawPolygon(coords: Coordinate[], setCurrentPoly: any,  API: any, map: any) {
-    if (coords.length === 0) {
-        return
+type SimpleMapProps = {
+  polys: Polygon[];
+  setPolys: Dispatch<SetStateAction<Polygon[]>>;
+};
+
+export function SimpleMap({ polys, setPolys }: SimpleMapProps) {
+  const defaultProps = {
+    center: {
+      lat: 50.9064509,
+      lng: 14.3204418,
+    },
+    zoom: 11,
+  };
+
+  const mapsAPIref: any = useRef(null);
+  const mapRef: any = useRef(null);
+
+  const [currentPoly, setCurrentPoly] = useState<Coordinate[]>([]);
+
+  function onClickOnMap({ lat, lng }: onClickProps) {
+    if (mapsAPIref.current === null || mapRef.current === null) {
+      return;
     }
 
-    const poly = new API.current.Polygon({
-        paths: coords,
-        strokeColor: "#FF0000",
-        strokeOpacity: 0.8,
-        strokeWeight: 2,
-        fillColor: "#FF0000",
-        fillOpacity: 0.35,
+    new mapsAPIref.current.Marker({
+      position: {
+        lat,
+        lng,
+      },
+      map: mapRef.current
     });
+    // setCorners((oldState: { lat: number, lng: number }[]) => [...oldState, { lat, lng }])
+    setCurrentPoly((oldState: { lat: number; lng: number }[]) => [
+      ...oldState,
+      { lat, lng },
+    ]);
+  }
+  type APIProps = {
+    map: any;
+    maps: any;
+    ref: any;
+  };
 
-    setCurrentPoly([]);
-    poly.setMap(map.current);
-}
+  function googleAPILoaded({ map, maps }: APIProps) {
+    mapsAPIref.current = maps;
+    mapRef.current = map;
+  }
 
-type Coordinate = {
-    lat: number
-    lng: number
-}
-
-export default function SimpleMap() {
-    const defaultProps = {
-        center: {
-            lat: 50.9064509,
-            lng: 14.3204418,
-        },
-        zoom: 11
-    };
-
-    const mapsAPIref: any = useRef(null);
-    const mapRef: any = useRef(null);
-
-    // const [corners, setCorners] = useState<{ lat: number, lng: number }[]>([]);
-    const [currentPoly, setCurrentPoly] = useState<Coordinate[]>([]);
-
-    function onClickOnMap({ lat, lng }: onClickProps) {
-        if (mapsAPIref.current === null || mapRef.current === null) {
-            return
+  return (
+    // Important! Always set the container height explicitly
+    <div style={{ height: "80vh", width: "60vw", marginBottom: "2em" }}>
+      <GoogleMapReact
+        onClick={onClickOnMap}
+        bootstrapURLKeys={{ key: "AIzaSyAj0-b3jl0qxB9vatFCSy9k5WlZsW_k7p4" }}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+        yesIWantToUseGoogleMapApiInternals
+        onGoogleApiLoaded={googleAPILoaded}
+      ></GoogleMapReact>
+      <button
+        onClick={() =>
+          createNewPoly(
+            currentPoly,
+            setCurrentPoly,
+            polys,
+            setPolys,
+            mapsAPIref,
+            mapRef
+          )
         }
+        style={{ margin: "1em" }}
+      >
+        Draw polygon
+      </button>
+    </div>
+  );
+}
 
-        const marker = new mapsAPIref.current.Marker({
-            position: {
-                lat, lng
-            },
-            map: mapRef.current,
-            title: "Marker " + (currentPoly.length + 1).toString()
-        })
-        // setCorners((oldState: { lat: number, lng: number }[]) => [...oldState, { lat, lng }])
-        setCurrentPoly((oldState: { lat: number, lng: number }[]) => [...oldState, { lat, lng }])
-    }
-    type APIProps = {
-        map: any
-        maps: any
-        ref: any
-    }
+function createNewPoly(
+  coords: Coordinate[],
+  setCurrentPoly: Dispatch<SetStateAction<Coordinate[]>>,
+  polys: Polygon[],
+  setPolys: Dispatch<SetStateAction<Polygon[]>>,
+  API: any,
+  mapRef: any
+) {
+  if (coords.length === 0) {
+    return;
+  }
 
-    function googleAPILoaded({ map, maps }: APIProps) {
-        mapsAPIref.current = maps
-        mapRef.current = map
-    }
+  const map = mapRef.current as google.maps.Map;
 
-    return (
-        // Important! Always set the container height explicitly
-        <div style={{ height: '80vh', width: '60vw', marginBottom: '2em' }}>
-            <GoogleMapReact
-                onClick={onClickOnMap}
-                bootstrapURLKeys={{ key: "AIzaSyAj0-b3jl0qxB9vatFCSy9k5WlZsW_k7p4" }}
-                defaultCenter={defaultProps.center}
-                defaultZoom={defaultProps.zoom}
-                yesIWantToUseGoogleMapApiInternals
-                onGoogleApiLoaded={googleAPILoaded}
-            >
-            </GoogleMapReact>
-            <button onClick={() => drawPolygon(currentPoly, setCurrentPoly, mapsAPIref, mapRef)} style={{ margin: "1em" }}>
-                Draw polygon
-            </button>
-        </div>
-    );
+  // draw polygon
+  new API.current.Polygon({
+    paths: coords,
+    strokeColor: "#FF0000",
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: "#FF0000",
+    fillOpacity: 0.35,
+    map,
+  });
+
+  // draw marker
+  new API.current.Marker({
+    position: getCenterOfPolygon(coords),
+    label: "Search Area " + (polys.length + 1).toString(),
+    map,
+  });
+
+  // save polygon to search list
+  const newPoly: Polygon = {
+    path: coords,
+    name: (polys.length + 1).toString(),
+    amntDrones: 0
+  };
+  setPolys((pols) => [...pols, newPoly]);
+  setCurrentPoly([]);
 }
